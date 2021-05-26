7589b9587c985a9df4df3583f5d3e0410a653571
Abdelrahman Emam <abdelrahmanemam91@gmail.com>
2021-05-26 23:10:40 +0200

    Added threading to crawler but not working 100%.

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,156 +1,187 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
+import org.jsoup.select.Elements;
 import Models.Website;
 
-
+enum STATUS {
+    UNTAKEN, TAKEN, CRAWLED
+}
 
 public class Crawler implements Runnable {
-    MySQLConnection mySQLConnection=new MySQLConnection();
-    List<Website>seeds;
-    List<Website> sites;
-    int seedsCount =0;
-    int totalNumberOfThreads=0;
-    Object lock=new Object();
-    Crawler(Object lock,int totalNumberOfThreads){
-        this.lock=lock;
-        this.totalNumberOfThreads=totalNumberOfThreads;
+    MySQLConnection mySQLConnection = new MySQLConnection();
+    int totalNumberOfThreads = 0;
+    static int crawlingLimit = 0;
+    List<Website> batchSizeQueue = new LinkedList<Website>();
+    List<String> extractedUrlsPerDocument = new LinkedList<String>();
+    Object lock;
+
+    Crawler(Object lock, int totalNumberOfThreads) {
+        this.lock = lock;
+        this.totalNumberOfThreads = totalNumberOfThreads;
     }
+
     @Override
     public void run() {
         startCrawling();
     }
 
-
-   
     public void startCrawling() {
-        int threadNumber=Integer.parseInt(  Thread.currentThread().getName());
-   //     try {
-       synchronized(lock){
-            seeds=SeedsController.retreiveSeeds(threadNumber,totalNumberOfThreads);
-            seedsCount= seeds.size();
-            System.out.println("I am thread: "+threadNumber +" My start is "+seeds.get(0).getId()+" and my end is: "+ seeds.get(seeds.size()-1).getId());
-       }            
-            // //LW MAWSELTESH LEL LIMIT BTA3Y W LESA 3ANDY 7AGAT NOT VISITED
-            // while (seedsCount!=50 && sites.size()!=0){
-            //     String siteUrl = sites.get(0).getUrl();
-            //     Document doc = Jsoup.connect(siteUrl).userAgent("Mozilla").get();
-            //     Elements links = doc.select("a");
-            //     links.forEach((link)->{
-            //         String urlString = link.attr("abs:href");
-            //         boolean checkRobotsTxt = checkRobots(urlString);
-            //         if (checkRobotsTxt) {
-            //         URL url;
-            //         try {
-            //             url = new URL(urlString);
-            //             //CHECK EL PROTOCOL 3LSHAN LW FEH MAILTO: BADAL HTTPS AW HTTP
-            //             if (url.getProtocol().equals("https") || url.getProtocol().equals("http")){
-                           
-            //                 //AT2AKED ENO MSH MAWGODA FEL DATABASE ALREADY
-            //                ArrayList<Website> temp; 
-            //                synchronized(lock){
-            //                         temp = mySQLConnection.retreiveWebsiteByUrl(urlString);
-            //                }
-                 
-            //                 if(temp.size()==0){
-            //                     //System.out.println(urlString);
-            //                     //ADD IT FEL DATABASE
-            //                     synchronized(lock){
-            //                             mySQLConnection.createWebsite(urlString, false);
-            //                     }
-                         
-            //                 }
-            //             }
-            //         } catch (MalformedURLException e) {
-            //             System.out.println(e.getMessage());
-            //         }   
-            //     }
-            //     });
-            //     //NE2LEB EL ISVISITED W NEZAWED EL seedsCount
-            //     synchronized(lock){
-            //         mySQLConnection.updateIsVisitedWebsiteById(sites.get(0).getId());
-            //     }
-            //     seedsCount+=1;
-            //     synchronized(lock){
-            //         sites = mySQLConnection.retreiveUncrawledWebsite();
-            //     }
-            // }
+        int threadNumber = Integer.parseInt(Thread.currentThread().getName());
 
+        try {
 
-            //(SORTED)WHILE THERE URLS WITH FALSE ISVISITED ATTRIBUTE || REACH LIMIT
-                //HANEMSEK AWEL WA7DA
-                //NEGEB EL BODY W NESHOF HAN3MEL BEH EH
-                //NEGEBE EL URLS
-                    //LKOL URL NCALL ROBOTS
-                    //LKOL URL NCHECK ENAHA MSH MAWGODA
-                    //LW MSH MAWGODA W EL ROBOTS AMAN NEZAWEDHA FEL DATABASE
-                //NE2LEB EL ISVISITED NE5ALEHA TRUE    
-            
-            
-        // }catch(MalformedURLException ex){
-        //     System.out.println(ex.getMessage());
-        
-        // } catch (IOException ex) {
+            /* Getting my seeds */
+            synchronized (lock) {
+                batchSizeQueue = SeedsController.retreiveSeeds(threadNumber, totalNumberOfThreads);
+                System.out.println("I am thread: " + threadNumber + " My start is " + batchSizeQueue.get(0).getId()
+                        + " and my end is: " + batchSizeQueue.get(batchSizeQueue.size() - 1).getId());
+            }
 
-        //     System.out.println(ex.getMessage());
-        // }
+            while (crawlingLimit < 9) {
+
+                while (batchSizeQueue.size() != 0 && crawlingLimit < 9) {
+                    System.out.println("Ana thread 5ara 3ala dma8i:" + threadNumber
+                            + " we da5alt dek om el loop welbatch 5ara size bt3y awl index feh el id bta3o: "
+                            + batchSizeQueue.get(0).getId());
+                    /* Get url of the first site in the queue */
+                    String siteUrl = batchSizeQueue.get(0).getUrl();
+                    /* Get html document of this website */
+                    Document doc = Jsoup.connect(siteUrl).userAgent("Mozilla").get();
+                    /* Get all aTags in this document */
+                    Elements aTags = doc.select("a");
+
+                    /*
+                     * Iterate on each aTag and get links in it and check for the robots file per
+                     * document
+                     */
+                    System.out.println("I am thread: " + threadNumber + " and i am extracting links from document");
+                    aTags.forEach((aTag) -> {
+
+                        /* Get absolute link in the aTag */
+                        String urlString = aTag.attr("abs:href");
+                        /* Check if this url valid for robots or not */
+                        boolean checkRobotsTxt = checkRobots(urlString);
+
+                        if (checkRobotsTxt) {
+                            URL url;
+                            try {
+                                /* Convert url to a Url object */
+                                url = new URL(urlString);
+
+                                /* Check the protocol */
+                                if (url.getProtocol().equals("https") || url.getProtocol().equals("http")) {
+                                    extractedUrlsPerDocument.add(urlString);
+                                }
+                            } catch (MalformedURLException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
+                    });
+
+                    System.out.println("I am thread: " + threadNumber + "and i finished extracting "
+                            + extractedUrlsPerDocument.size() + " link");
+
+                    System.out.println("I am thread: " + threadNumber + " and i want to acquire the lock");
+
+                    synchronized (lock) {
+
+                        //
+                        System.out.println("I am thread: " + threadNumber + " and i acquired the lock");
+                        /*
+                         * Add the extracted urls to the database
+                         */
+
+                        for (int i = 0; i < extractedUrlsPerDocument.size(); i++) {
+                            if (mySQLConnection.createWebsite(extractedUrlsPerDocument.get(i),
+                                    STATUS.UNTAKEN.ordinal())) {
+                                System.out.println(
+                                        "The url: " + extractedUrlsPerDocument.get(i) + " is added to the database.");
+                            }
+                        }
+
+                        /* Empty the extractedUrlsPerDocument */
+                        extractedUrlsPerDocument.clear();
+                        /*
+                         * Update the current working document to crawled and download it and remove it
+                         * from batch queue
+                         */
+
+                        if (mySQLConnection.updateStatusOfWebsiteById(batchSizeQueue.get(0).getId(),
+                                STATUS.CRAWLED.ordinal())) {
+                            System.out.println("I am thread: " + threadNumber + " and url: "
+                                    + batchSizeQueue.get(0).getUrl() + " status is changed to crawled to database.");
+                            downloadAndSave(Integer.toString(batchSizeQueue.get(0).getId()), doc);
+                            batchSizeQueue.remove(0);
+                            crawlingLimit += 1;
+                            System.out.println(
+                                    "I am thread: " + threadNumber + " and the crawling limit is: " + crawlingLimit);
+                        }
+                        /* Increment Crawling limit */
+                        System.out.println("I am thread: " + threadNumber + " and i released the lock");
+
+                    }
+                    System.out.println("Ana thread 5ara 3ala dma8i:" + threadNumber
+                            + " we 5aragt mn dek om el loop welbatch 5ara size bt3y awl index feh el id bta3o: "
+                            + batchSizeQueue.get(0).getId());
+                }
+                /* Retreive another batch of websites */
+            }
+
+            System.out.println("I am thread: " + threadNumber + " we 5alast");
+        } catch (MalformedURLException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
-     public boolean checkRobots(String url) {
+    public boolean checkRobots(String url) {
         try {
-            if(url.contains("#"))
-            {
+            if (url.contains("#")) {
                 return false;
             }
             URL urlObj = null;
             urlObj = new URL(url);
             String path = urlObj.getPath();
-            if(path.contains("#"))
-            {
+            if (path.contains("#")) {
                 return false;
             }
-                
+
             String robotsFileUrl = urlObj.getProtocol() + "://" + urlObj.getHost() + "/robots.txt";
             Document doc = Jsoup.connect(robotsFileUrl).ignoreContentType(true).userAgent("Mozilla").get();
             String robotsText = doc.text();
             int index = robotsText.indexOf("User-Agent: *");
-            if(index == -1)
-            {
+            if (index == -1) {
                 index = robotsText.indexOf("User-agent: *");
-                if(index == -1) return false;
+                if (index == -1)
+                    return false;
             }
             String sub = robotsText.substring(index);
 
             String[] robotTextArray = sub.split(" ");
-            for(int i =3 ; i< robotTextArray.length ; i+=2)
-            {
+            for (int i = 3; i < robotTextArray.length; i += 2) {
 
-                if((robotTextArray[i-1]).equals( "Disallow:"))
-                {
-                    if((robotTextArray[i]).equals( "/")) //All are disallowed
+                if ((robotTextArray[i - 1]).equals("Disallow:")) {
+                    if ((robotTextArray[i]).equals("/")) // All are disallowed
                     {
                         return false;
                     }
-                        
-                    if(!path.equals("/"))
-                    {
-                        if((robotTextArray[i]).contains(path))
-                        {
+
+                    if (!path.equals("/")) {
+                        if ((robotTextArray[i]).contains(path)) {
                             return false;
                         }
                     }
-                }
-                else if((robotTextArray[i-1]).equals( "Allow:"))
-                {
-                    //check if it is allowed -> return true
-                }
-                else
-                {
+                } else if ((robotTextArray[i - 1]).equals("Allow:")) {
+                    // check if it is allowed -> return true
+                } else {
                     break;
                 }
 
@@ -163,34 +194,23 @@ public class Crawler implements Runnable {
 
     }
 
-    //THIS FUNCTION IS BOOLEAN FOR ONLY 1 REASON IF IT FAILED TO CONNECT TO THE GIVEN URL IT WILL RETURN FALSE
-    public boolean downloadAndSave(String fileName , String inUrl)
-    {
-        
-        Document doc;
+    // THIS FUNCTION IS BOOLEAN FOR ONLY 1 REASON IF IT FAILED TO CONNECT TO THE
+    // GIVEN URL IT WILL RETURN FALSE
+    public boolean downloadAndSave(String fileName, Document doc) {
         try {
-            doc = Jsoup.connect(inUrl).ignoreContentType(true).userAgent("Mozilla").get();
             String htmlContent = doc.html();
-            File output = new File(fileName + ".html");
+            File output = new File("./src/Documents/" + fileName + ".html");
             FileWriter writer = new FileWriter(output);
-
             writer.write(htmlContent);
             writer.flush();
             writer.close();
 
-
         } catch (IOException e) {
-            
             e.printStackTrace();
             System.out.println("Error in Connection!");
             return false;
         }
         return true;
-        
-
     }
-
-
- 
 
 }
--- src/MySQLConnection.java
+++ src/MySQLConnection.java
@@ -15,13 +15,14 @@ public class MySQLConnection {
     private Connection connectToMySQLDatabase() throws Exception {
         try {
             // This will load the MySQL driver, each DB has its own driver
-            Class.forName("com.mysql.cj.jdbc.Driver");   
+            Class.forName("com.mysql.cj.jdbc.Driver");
             // Setup the connection with the DB
-            if(connect==null){
-                    connect = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/fbmV4A0g3B","fbmV4A0g3B","nKVPcIUaOD");
-                    createCrawlerTable();
+            if (connect == null) {
+                connect = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/fbmV4A0g3B", "fbmV4A0g3B",
+                        "nKVPcIUaOD");
+                createCrawlerTable();
             }
-        return connect;
+            return connect;
         } catch (Exception e) {
             throw e;
         }
@@ -33,11 +34,11 @@ public class MySQLConnection {
             Connection myConnection = connectToMySQLDatabase();
             preStatement = myConnection.prepareStatement("SHOW TABLES LIKE 'Crawler';");
             resultSet = preStatement.executeQuery();
-          
-                preStatement = myConnection.prepareStatement(
-                        "CREATE TABLE `Crawler` ( `id` int(11) NOT NULL AUTO_INCREMENT, `url` varchar(255) DEFAULT NULL,`status` TINYINT(2), PRIMARY KEY (`id`)) ENGINE=InnoDB;");
-                result = preStatement.execute();
-                this.close();
+
+            preStatement = myConnection.prepareStatement(
+                    "CREATE TABLE `Crawler` ( `id` int(11) NOT NULL AUTO_INCREMENT, `url` varchar(255) DEFAULT NULL,`status` TINYINT(2), PRIMARY KEY (`id`)) ENGINE=InnoDB;");
+            result = preStatement.execute();
+            this.close();
             return result;
         } catch (Exception e) {
             System.out.println(e.toString());
@@ -46,44 +47,42 @@ public class MySQLConnection {
         }
     }
 
-
-
-    public void createWebsite(String url,int status) {
-        try{
-            Connection myConnection=connectToMySQLDatabase();
-   
-            boolean result;
-            preStatement = myConnection.prepareStatement("INSERT INTO Crawler (url, status) VALUES (?,?);",
+    public boolean createWebsite(String url, int status) {
+        boolean result = false;
+        try {
+            Connection myConnection = connectToMySQLDatabase();
+            preStatement = myConnection.prepareStatement("INSERT IGNORE INTO Crawler (url, status) VALUES (?,?);",
                     ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            preStatement.setString(1,url);
-            preStatement.setInt(2,status);
+            preStatement.setString(1, url);
+            preStatement.setInt(2, status);
             result = preStatement.execute();
-            //System.out.println(result);
-        }catch(Exception e){
+            return !result;
+        } catch (Exception e) {
             System.out.println(e.toString());
         }
-       
+        return result;
+
     }
-    
-    public ArrayList<Website> retreiveUncrawledWebsite() {
+
+    public ArrayList<Website> retreiveUncrawledWebsite(int status) {
         try {
             Connection myConnection = connectToMySQLDatabase();
             int size = 0;
-            preStatement = myConnection.prepareStatement("SELECT * FROM Crawler E where E.status = 0",
+            preStatement = myConnection.prepareStatement("SELECT * FROM Crawler E where E.status = ?",
                     ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            preStatement.setInt(1, status);
             resultSet = preStatement.executeQuery();
             if (resultSet != null) {
-                resultSet.beforeFirst();  
-                resultSet.last(); 
- 
-                size = resultSet.getRow(); 
+                resultSet.beforeFirst();
+                resultSet.last();
+                size = resultSet.getRow();
                 resultSet.beforeFirst();
                 ArrayList<Website> uncrawledSites = new ArrayList<>(size);
                 while (resultSet.next()) {
-                    Website temp=new Website();
+                    Website temp = new Website();
                     temp.setId(resultSet.getInt("id"));
                     temp.setUrl(resultSet.getString("url"));
-                    temp.setIsVisited(resultSet.getBoolean("status"));
+                    temp.setStatus(resultSet.getInt("status"));
                     uncrawledSites.add(temp);
                 }
                 this.close();
@@ -98,13 +97,13 @@ public class MySQLConnection {
         return null;
 
     }
-    
+
     public ArrayList<Website> retreiveWebsiteByUrl(String url) {
         try {
             Connection myConnection = connectToMySQLDatabase();
             int size = 0;
-            preStatement = myConnection.prepareStatement("SELECT * FROM Crawler E where E.url = ?" , ResultSet.TYPE_SCROLL_SENSITIVE, 
-                        ResultSet.CONCUR_UPDATABLE);
+            preStatement = myConnection.prepareStatement("SELECT * FROM Crawler E where E.url = ?",
+                    ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
             preStatement.setString(1, url);
             resultSet = preStatement.executeQuery();
             if (resultSet != null) {
@@ -116,7 +115,7 @@ public class MySQLConnection {
                     Website temp = new Website();
                     temp.setId(resultSet.getInt("id"));
                     temp.setUrl(resultSet.getString("url"));
-                    temp.setIsVisited(resultSet.getBoolean("isVisited"));
+                    temp.setStatus(resultSet.getInt("status"));
                     uncrawledSites.add(temp);
                 }
                 this.close();
@@ -124,40 +123,41 @@ public class MySQLConnection {
             }
         } catch (Exception e) {
             System.out.println(e.toString());
-        
+
         }
         this.close();
         return null;
     }
 
-    public boolean updateIsVisitedWebsiteById(int id) {
+    public boolean updateStatusOfWebsiteById(int id, int status) {
         boolean result = false;
         try {
             Connection myConnection = connectToMySQLDatabase();
-            preStatement = myConnection.prepareStatement("UPDATE Crawler SET isVisited = true WHERE id = ?",
+            preStatement = myConnection.prepareStatement("UPDATE Crawler SET status = ? WHERE id = ?",
                     ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            preStatement.setInt(1, id);
+            preStatement.setInt(1, status);
+            preStatement.setInt(2, id);
             result = preStatement.execute();
             this.close();
-            return result;
+            return !result;
         } catch (Exception e) {
             System.out.println(e.toString());
             this.close();
-            return result;
+            return false;
         }
     }
 
     // private void writeResultSet(ResultSet resultSet) throws SQLException {
 
-    //     while (resultSet.next()) {
+    // while (resultSet.next()) {
 
-    //         int id = resultSet.getInt("id");
-    //         boolean isVisited = resultSet.getBoolean("isVisited");
-    //         System.out.print("id: "+ id);
-    //         System.out.print(" ");
-    //         System.out.println("isVisited: " + isVisited);
+    // int id = resultSet.getInt("id");
+    // boolean isVisited = resultSet.getBoolean("isVisited");
+    // System.out.print("id: "+ id);
+    // System.out.print(" ");
+    // System.out.println("isVisited: " + isVisited);
 
-    //     }
+    // }
     // }
 
     // You need to close the resultSet
